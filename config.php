d3851762255177fd60ce5f917705f3d45e8cc30d
kelvin-kelvin <67065100+kelvin-kelvin@users.noreply.github.com>
2020-06-17 22:46:26 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+$google_client = new Google_Client();
+$google_client->setAuthConfig('client_secret.json');
+$google_client->setRedirectUri('http://localhost/apiGoogle/index.php');
+$google_client->addScope('email');
+$google_client->addScope('profile');
+session_start();
+?>
